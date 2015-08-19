bf154a9eab71165f1857c480f0f064aa06ae9bde
ogarces <lettertoolia@gmail.com>
2015-08-19 17:31:01 +0200

    pras2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The function makeCacheMatrix
+## Sets the value of the matrix
+## Gets the value of the matrix
+## Sets the value of the inverse of the matrix
+## Gets the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates the value of the inverse of the matrix.
+## the matrix must be invertible: dimensions n*n with det!=0
+## det can be verified by running det(matrix)
+## if the value has been calculated previously, the function returns the cached value.
+
+## test:
+## create a n*n matrix mt <- matrix(sample(100, 25), 5, 5)
+## pass mt as argument to makeCacheMatrix mc <- makeCacheMatrix(mt)
+## pass mc as argument to cacheSolve s <- cacheSolve(mc)
+## executing cacheSolve(mc) for the first time returns the inverse of mc
+## executing cacheSolve(mc) for the second time returns a message:
+## "getting cached matrix"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached matrix")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
